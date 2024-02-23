8b6a22a5adf7ce35a3cfe02a84f978e53e3877c7
montgomeryheath <101486099+montgomeryheath@users.noreply.github.com>
2024-02-22 20:55:56 -0500

    final
    final working project

--- .ipynb_checkpoints/mean_var_std-checkpoint.py
+++ .ipynb_checkpoints/mean_var_std-checkpoint.py
@@ -5,14 +5,13 @@ def calculate(list):
         raise ValueError('List must contain nine numbers.')
         
     array = np.reshape(list, (3, 3))
-    print(array)
-
-    min = [np.min(array, axis=0), np.min(array, axis=1), np.min(array)]
-    max = [np.max(array, axis=0), np.max(array, axis=1), np.max(array)]
-    sum = [np.sum(array, axis=0), np.sum(array, axis=1), np.sum(array)]
-    mean = [np.mean(array, axis=0), np.mean(array, axis=1), np.mean(array)]
-    variance = [np.var(array, axis=0), np.var(array, axis=1), np.var(array)]
-    stddev = [np.std(array, axis=0), np.std(array, axis=1), np.std(array)]
+ 
+    min = [np.min(array, axis=0).tolist(), np.min(array, axis=1).tolist(), np.min(array).tolist()]
+    max = [np.max(array, axis=0).tolist(), np.max(array, axis=1).tolist(), np.max(array).tolist()]
+    sum = [np.sum(array, axis=0).tolist(), np.sum(array, axis=1).tolist(), np.sum(array).tolist()]
+    mean = [np.mean(array, axis=0).tolist(), np.mean(array, axis=1).tolist(), np.mean(array).tolist()]
+    variance = [np.var(array, axis=0).tolist(), np.var(array, axis=1).tolist(), np.var(array).tolist()]
+    stddev = [np.std(array, axis=0).tolist(), np.std(array, axis=1).tolist(), np.std(array).tolist()]
         
     calculations = dict([
          ('mean', mean), 
@@ -22,8 +21,6 @@ def calculate(list):
          ('min', min), 
          ('sum',sum)
         ])
-    print(calculations)
+    
 
     return calculations
-
-calculate([0,1,2,3,4,5,6,7,8])
